7917cef24977f7c56bce41527a879c82088681b8
Wenzel Lowe <brotherwenzel@gmail.com>
2019-12-12 10:11:54 -0800

    mad world

--- src/apiRequests.js
+++ src/apiRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://attendance-go.herokuapp.com/api';
-const API_URL = 'http://localhost:1444/api';
+const API_URL = 'https://attendance-go.herokuapp.com/api';
+// const API_URL = 'http://localhost:1444/api';
 
 export const getGroups = async token => {
   const res = await axios.get(`${API_URL}/groups?token=${token}`);
